19479fd458fbb514ac6dad9a81952a256e48e6ee
Navneet Kumar <navneetk@bluejeans.com>
2016-10-26 00:53:01 +0530

    reverting runtime module

--- runtime/src/main/java/org/lambadaframework/runtime/Handler.java
+++ runtime/src/main/java/org/lambadaframework/runtime/Handler.java
@@ -18,74 +18,58 @@ import java.nio.charset.Charset;
 
 import org.apache.log4j.Logger;
 
+public class Handler
+        implements RequestHandler<Request, Response> {
+
+    static final Logger logger = Logger.getLogger(Handler.class);
+
+    private Router router;
 
 
-
-public class Handler implements RequestStreamHandler {
-	
-	static final Logger logger = Logger.getLogger(Handler.class);
-	
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
-        logger.debug("Handling 1 request started = " + context);
-        String response =  "Hello proxy";
-        outputStream.write(response.getBytes(Charset.forName("UTF-8")));
+    public Handler setRouter(Router router) {
+        this.router = router;
+        return this;
     }
 
-}
+    public Router getRouter() {
+        if (router != null) {
+            return router;
+        }
+        return Router.getRouter();
+    }
 
-//public class Handler
-//        implements RequestHandler<Request, Response> {
-//
-//    static final Logger logger = Logger.getLogger(Handler.class);
-//
-//    private Router router;
-//
-//
-//    public Handler setRouter(Router router) {
-//        this.router = router;
-//        return this;
-//    }
-//
-//    public Router getRouter() {
-//        if (router != null) {
-//            return router;
-//        }
-//        return Router.getRouter();
-//    }
-//
-//    /**
-//     * If request object's "method" field is null or has an invalid
-//     * HTTP method string it is impossible to process the request
-//     * thus we throw an exception and 500 HTTP error.
-//     *
-//     * @param requestObject Request object
-//     * @throws Exception
-//     */
-//    private void checkHttpMethod(Request requestObject)
-//            throws Exception {
-//        if (requestObject.getMethod() == null) {
-//            throw new Exception("Method was null");
-//        }
-//    }
-//
-//
-//    @Override
-//    public Response handleRequest(Request request, Context context) {
-//
-//        try {
-//            logger.debug("Request started with " + request + " and " + context);
-//
-//            checkHttpMethod(request);
-//            logger.debug("Request check is ok.");
-//
-//            logger.debug("Matching request to a resource handler.");
-//            ResourceMethod matchedResourceMethod = getRouter().route(request);
-//
-//            logger.debug("Returning result.");
-//            return Response.buildFromJAXRSResponse(ResourceMethodInvoker.invoke(matchedResourceMethod, request, context));
-//        } catch (Exception ex) {
-//            return ErrorHandler.getErrorResponse(ex);
-//        }
-//    }
-//}
+    /**
+     * If request object's "method" field is null or has an invalid
+     * HTTP method string it is impossible to process the request
+     * thus we throw an exception and 500 HTTP error.
+     *
+     * @param requestObject Request object
+     * @throws Exception
+     */
+    private void checkHttpMethod(Request requestObject)
+            throws Exception {
+        if (requestObject.getMethod() == null) {
+            throw new Exception("Method was null");
+        }
+    }
+
+
+    @Override
+    public Response handleRequest(Request request, Context context) {
+
+        try {
+            logger.debug("Request started with " + request + " and " + context);
+
+            checkHttpMethod(request);
+            logger.debug("Request check is ok.");
+
+            logger.debug("Matching request to a resource handler.");
+            ResourceMethod matchedResourceMethod = getRouter().route(request);
+
+            logger.debug("Returning result.");
+            return Response.buildFromJAXRSResponse(ResourceMethodInvoker.invoke(matchedResourceMethod, request, context));
+        } catch (Exception ex) {
+            return ErrorHandler.getErrorResponse(ex);
+        }
+    }
+}
