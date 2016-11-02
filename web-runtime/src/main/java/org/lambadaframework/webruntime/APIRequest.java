ccabb14dbe8b498903c37dc8fc9eb3a8d1a8d3bd
Navneet Kumar <navneetk@bluejeans.com>
2016-11-03 01:28:02 +0530

    adding headers and body

--- web-runtime/src/main/java/org/lambadaframework/webruntime/APIRequest.java
+++ web-runtime/src/main/java/org/lambadaframework/webruntime/APIRequest.java
@@ -1,8 +1,11 @@
 package org.lambadaframework.webruntime;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import org.eclipse.jetty.client.HttpRequest;
+
 import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.util.StringContentProvider;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -13,6 +16,10 @@ public class APIRequest {
 	private String path;
 	private String httpMethod;
 	private Map<String, String> headers;
+
+	private Map<String, String> queryStringParameters;
+	private String body;
+	
 	public String getHttpMethod() {
 		return httpMethod;
 	}
@@ -45,11 +52,6 @@ public class APIRequest {
 		this.body = body;
 	}
 
-	private Map<String, String> queryStringParameters;
-	private String body;
-	
-	
-
 	public String getPath() {
 		return path;
 	}
@@ -67,18 +69,28 @@ public class APIRequest {
 	}
 	
 	public Request copyRequest(Request request) {
-		Request updatedRequest = request.method(httpMethod).path(path);
+		Request updatedRequest = request
+				.method(httpMethod)
+				.path(path)
+				.content(new StringContentProvider(body));
+
 		for (Map.Entry<String, String> header : headers.entrySet())
 		{
 		    updatedRequest.header(header.getKey(), header.getValue());
 		}
+		
+		for (Map.Entry<String, String> queryParam : queryStringParameters.entrySet())
+		{
+			updatedRequest.param(queryParam.getKey(), queryParam.getValue());
+		}
+		
 		return updatedRequest;
 	}
 	
 	@Override
     public String toString() {
         return "Request[ " +
-                "path='" + path + '\'' +
+                "path='" +  path + '\'' +
                 ", resource=" + resource +
                 ", httpMethod=" + httpMethod +
                  ", headers=" + headers +
